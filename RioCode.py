acd53f58d787ebb9c379357c522d48dde085fb86
moticilla <ellie.pizey@gmail.com>
2023-11-10 18:39:26 +0000

    initial commit

--- RioCode.py
+++ RioCode.py
@@ -1,5 +1,23 @@
 from random import randrange
+import numpy as np
 #Variables:
-Mi_range = [60,100]
-Mi = randrange(Mi_range[0],Mi_range[1])
-print(Mi)
+delta_t = 0.2
+t = 0
+t_max = 5
+k = 3.15
+a = 9.2
+w = 1.3
+n = 3
+r = np.zeros(n, len(t_set))
+phi = np.zeros(n, len(t_set))
+def d(i,p):
+    return np.sqrt((r[p][1]-r[i][1])**2 + (r[p][1]-r[i][1])**2)
+t_set = np.arange(0, t_max, delta_t)
+
+def w(i,p):
+    return (a/(e**(w*d(i,p)+a))
+
+for p in range(n):
+    for t in range(len(t_set)):
+        w_i = 
+        phi[p][t+1] = phi[p][t] - delta_t*k/n #sum
