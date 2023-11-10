617b56c1fa02d937b59a476d00826770988e4867
moticilla <ellie.pizey@gmail.com>
2023-11-10 19:05:56 +0000

    Onwards

--- RioCode.py
+++ RioCode.py
@@ -8,16 +8,17 @@ k = 3.15
 a = 9.2
 w = 1.3
 n = 3
+t_set = np.arange(0, t_max, delta_t)
 r = np.zeros(n, len(t_set))
 phi = np.zeros(n, len(t_set))
 def d(i,p):
     return np.sqrt((r[p][1]-r[i][1])**2 + (r[p][1]-r[i][1])**2)
-t_set = np.arange(0, t_max, delta_t)
 
 def w(i,p):
-    return (a/(e**(w*d(i,p)+a))
+    return ( a/(np.exp(w*d(i,p)+a)) )
 
 for p in range(n):
     for t in range(len(t_set)):
-        w_i = 
-        phi[p][t+1] = phi[p][t] - delta_t*k/n #sum
+        for i in range(n):
+            w_i = w(i,p)
+            phi[p][t+1] = phi[p][t] - delta_t*k/n * w_i * np.sin(phi[i] - phi[p])
