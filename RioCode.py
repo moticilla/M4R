0f12b2147ef40f266caccd6da3b4f38175acc449
moticilla <ellie.pizey@gmail.com>
2023-11-20 17:58:29 +0000

    Onwards

--- RioCode.py
+++ RioCode.py
@@ -19,7 +19,7 @@ for i in range(n):
 phi = np.zeros((len(t_set),n))
 r = np.zeros((len(t_set),n))
 def d(i,p,t):
-    return np.sqrt((position[t][p][0]-position[t][i][0])**2 + (position[t][p][1]-position[t][i][1])**2)
+    return np.sqrt((position[t,p,0]-position[t,i,0])**2 + (position[t,p,1]-position[t,i,1])**2)
 def angle(i,p,t):
     return phi[t,p]-phi[t,i]
 def w(i,p,t):
@@ -30,29 +30,28 @@ def w(i,p,t):
 
 for t in range(1, len(t_set)):
     for p in range(n):
-        r[t][p]   = r[t-1][p]
-        phi[t][p] = phi[t-1][p]
+        r[t,p]   = r[t-1,p]
+        phi[t,p] = phi[t-1,p]
         for i in range(n):
             localN = 0
             phi_sum = 0
             r_sum = 0
             dis = d(i,p,t-1)
             ang = angle(i,p,t-1)
-            if (dis< d_max) and (np.absolute(ang)<angle_max) and (i != p):
+            if (dis< d_max) and (np.absolute(ang)<angle_max) and (i != p):#the angle condition is wrong
                 localN += 1
                 w_i = w(i,p,t-1)
-                phi_sum += w_i * np.sin(phi[t-1][i] - phi[t-1][p])
-                r_sum           += w_i *(r[t-1][i]-r[t-1][p])
+                phi_sum += w_i * np.sin(phi[t-1,i] - phi[t-1,p])
+                r_sum           += w_i *(r[t-1,i]-r[t-1,p])
         if localN >0:
-            r[t][p]   = r[t-1][p] + delta_t * (c/localN) * r_sum
-            phi[t][p] = phi[t-1][p] - delta_t * (k/localN) * phi_sum
+            r[t,p]   = r[t-1,p] + delta_t * (c/localN) * r_sum
+            phi[t,p] = phi[t-1,p] - delta_t * (k/localN) * phi_sum
         distance = r * delta_t
-        position[t][p][0] = position[t-1][p][0] + distance[t-1][p]*np.cos(phi[t-1][p])
-        position[t][p][1] = position[t-1][p][1] + distance[t-1][p]*np.sin(phi[t-1][p])
-plt.plot(position[:][1][0],position[:][1][1] )
-print(position[:][1][0])
-print(len(t_set))
-print(np.shape(position))
+        position[t,p,0] = position[t-1,p,0] + distance[t-1,p]*np.cos(phi[t-1,p])
+        position[t,p,1] = position[t-1,p,1] + distance[t-1,p]*np.sin(phi[t-1,p])
+
+#plt.plot(position[:,1,0],position[:,1,1] )
 #plt.show()
-print(position.max(),position.min(),position[:,0,0])
-print(position[:][0][0])
+print(position[:,0,:])
+print(position[:,1,:])
+print(position[:,2,:])
