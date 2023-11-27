486227796020ba1c1b44cb9e0dbfd7f88b43ee2b
moticilla <ellie.pizey@gmail.com>
2023-11-27 14:53:09 +0000

    Angle consition added

--- RioCode.py
+++ RioCode.py
@@ -4,26 +4,27 @@ import matplotlib.pyplot as plt
 #Variables:
 delta_t = 0.2
 t_max = 6
-n = 3
+n = 10
 d_max = 4
-angle_max = 90
+angle_max = 1.57 #pi/2
 k = 3.15
 a = 9.2
 w_var = 1.3
 c = 3.61
-t_set = np.arange(0, t_max, delta_t)
+t_set    = np.arange(0, t_max, delta_t)
 position = np.zeros((len(t_set), n, 2))
-phi = np.zeros((len(t_set),n))
-r = np.zeros((len(t_set),n))
+phi      = np.zeros((len(t_set),n))
+r        = np.zeros((len(t_set),n))
 for i in range(n):
     position[0,i,0] = 0.5*random()
     position[0,i,1] = 0.5*random()
     phi[0,i] = 3* random()
-    r[0,i] = 0.3* random()
+    r[0,i]   = 0.3* random()
 def d(i,p,t):
     return np.sqrt((position[t,p,0]-position[t,i,0])**2 + (position[t,p,1]-position[t,i,1])**2)
 def angle(i,p,t):
-    return phi[t,p]-phi[t,i]
+    theta = np.arctan((position[t,i,1]-position[t,p,1])/ (position[t,i,0] - position[t,p,0]))
+    return phi[t,p]-theta
 def w(i,p,t):
     dis = d(i,p,t)
     return ( a/(np.exp(w_var * dis)+a) )
@@ -38,8 +39,7 @@ for t in range(1, len(t_set)):
         for i in range(n):
             dis     = d(i,p,t-1)
             ang     = angle(i,p,t-1)
-            #if (dis< d_max) and (np.absolute(ang)<angle_max) and (i != p):#the angle condition is wrong
-            if (dis< d_max) and (i != p):
+            if (dis< d_max) and (np.absolute(ang)<angle_max) and (i != p):#the angle condition is wrong
                 localN  += 1
                 w_i      = w(i,p,t-1)
                 phi_sum += w_i * np.sin(phi[t-1,i] - phi[t-1,p])
@@ -51,8 +51,11 @@ for t in range(1, len(t_set)):
         position[t,p,0] = position[t-1,p,0] + distance[t-1,p]*np.cos(phi[t-1,p])
         position[t,p,1] = position[t-1,p,1] + distance[t-1,p]*np.sin(phi[t-1,p])
 
-plt.scatter(position[:,0,0],position[:,0,1])
-plt.scatter(position[:,1,0],position[:,1,1])
-plt.scatter(position[:,2,0],position[:,2,1])
+#for i in range(n):
+#    plt.scatter(position[:,i,0],position[:,i,1])
+
+for i in range(n):
+    plt.scatter(position[:,i,0],position[:,i,1])
+
 plt.show()
 #print(position[:,0,:])
