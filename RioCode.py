9aeed1188889e5e118cafd26e87259fda424a056
moticilla <ellie.pizey@gmail.com>
2023-12-11 15:06:38 +0000

    Add initial position for exp1

--- RioCode.py
+++ RioCode.py
@@ -6,9 +6,10 @@ from scipy import special
 #Variables:
 delta_t = 0.2
 t_max = 6
-n = 7
+n = 12
 d_max = 4
-angle_max = 1.57 #pi/2
+pi = 3.14
+angle_max = pi/2
 k = 3.15
 a = 9.2
 w_var = 1.3
@@ -17,11 +18,23 @@ t_set    = np.arange(0, t_max, delta_t)
 position = np.zeros((len(t_set), n, 2))
 phi      = np.zeros((len(t_set),n))
 r        = np.zeros((len(t_set),n))
+#set position of 12 neighbours
+for i in range(5):
+    position[0,i,0] = 1.5*np.sin(pi/4 + i*pi/8)
+    position[0,i,1] = 1.5*np.cos(pi/4 + i*pi/8)
+for i in range(5,12):    
+    position[0,i,0] = 3.5*np.sin(pi/4 + (i-5)*pi/12)
+    position[0,i,1] = 3.5*np.cos(pi/4 + (i-5)*pi/12)
+print(position[0,:,:])
+plt.scatter(position[0,:,0], position[0,:,1])
+plt.show()
+
+'''
 for i in range(n):
     position[0,i,0] = 0.5*random()
     position[0,i,1] = 0.5*random()
     phi[0,i] = 3* random()
-    r[0,i]   = 0.3* random()
+    r[0,i]   = 0.3* random()'''
 def d(i,p,t):
     return np.sqrt((position[t,p,0]-position[t,i,0])**2 + (position[t,p,1]-position[t,i,1])**2)
 def angle(i,p,t):
@@ -72,5 +85,6 @@ for i in range(n):
 exp1_LateralDeviation_strip = pd.read_csv('/home/ellie/Documents/M4R/exp1/Exp1_LateralDeviation_strip.txt', sep="\s+", skip_blank_lines=True, 
                    header=None, skiprows = [0,1,12,15,16,17,18, 29,32,33,34,35,46])
 exp1_LateralDeviation_strip.columns = ["Subject", "Near 0", "Far 0", "Near 3", "Near 6", "Near 9", "Near 12", "Far 3", "Far 6", "Far 9", "Far 12"]
-exp1_LateralDeviation_strip.drop([10,13,14,25,28,29,40])
+exp1_LateralDeviation_strip.drop([10,13,14,25,28,29,40], axis = 0)
 #print(exp1_LateralDeviation_strip)
+#multiply left turn by -1 and add to right turns
