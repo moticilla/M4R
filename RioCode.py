fe73a4ac8c4b228ba57b982173f763963b58c757
moticilla <ellie.pizey@gmail.com>
2024-01-16 14:51:06 +0000

    perturb three neighbours

--- RioCode.py
+++ RioCode.py
@@ -6,7 +6,7 @@ from scipy import special
 #Variables:
 delta_t = 0.2
 t_max = 60
-n = 30
+n = 31
 d_max = 4
 pi = 3.14
 angle_max = pi/2
@@ -17,7 +17,7 @@ c = 3.61
 t_set    = np.arange(0, t_max, delta_t)
 position = np.zeros((len(t_set), n, 2))
 phi      = np.zeros((len(t_set),n))
-r        = np.zeros((len(t_set),n))
+r        = np.zeros((len(t_set),n)) #this is r dot not r
 def d(i,p,t):
     return np.sqrt((position[t,p,0]-position[t,i,0])**2 + (position[t,p,1]-position[t,i,1])**2)
 def angle(i,p,t):
@@ -29,25 +29,29 @@ def w(i,p,t):
 def ogive(mu, sigma,t):
     return 0.5 *(1+special.erf((t-mu)/sigma*np.sqrt(2)))
 def perturb(S, t, Heading = True, Speed = False):
-    #S is the set of virtual neighbours to be perturbed either by speed or by direction
+    #S is the set of virtual neighbours to be perturbed either by speed or by heading at time t
     sign = choice([1,-1])
-    for i in range(int(0.5//delta_t)):
-        if Heading == True:
-            phi[t+i] = phi[t] + sign*(10/360)*2*pi*ogive(0,0.083,i*delta_t-0.25)
-        if Speed == True:
-            r[t+i] = r[t] + 0.3*sign*ogive(0,0.083,i*delta_t-0.25)
+    for ii in S:
+        i = S[ii]
+        for t_step in range(int(0.5//delta_t)):
+            if Heading == True:
+                phi[t+t_step,i] = phi[t,i] + sign*(10/360)*2*pi*ogive(0,0.083,t_step*delta_t-0.25)
+            if Speed   == True:
+                r[t+t_step,i]   = r[t,i]   + sign*0.3*ogive(0,0.083,t_step*delta_t-0.25)
 
-#set position of 30 neighbours for experiment 1 in two circles
+#set position of 30 neighbours for experiment 1 in two circles with real person in the middle
 for i in range(14):
     position[0,i,0] = 1.5*np.sin(i*pi/7)
     position[0,i,1] = 1.5*np.cos(i*pi/7)
 for i in range(14,30):    
     position[0,i,0] = 3.5*np.sin((i-14)*pi/8)
     position[0,i,1] = 3.5*np.cos((i-14)*pi/8)
-plt.scatter(position[0,:,0], position[0,:,1])
-for i in range(30):
-    plt.annotate(i, (position[0,i,0], position[0,i,1]))
-plt.show()
+position[0,30,0] = 0
+position[0,30,1] = 0
+#plt.scatter(position[0,:,0], position[0,:,1])
+#for i in range(31):
+#    plt.annotate(i, (position[0,i,0], position[0,i,1]))
+#plt.show()
 
 #make the 18 neighbours not in the origional view jitter
 for t in range(1, len(t_set)):
@@ -59,9 +63,26 @@ for t in range(1, len(t_set)):
         position[t,i,1] = normalvariate(3.5,0.15)*np.cos(normalvariate((i-14)*pi/8,(8/360)*2*pi))
 
 #make all virtual people speed up to 1.3m/s in 3 s
-for i in range(3):
+for i in range(30):
     for t in range(int(3//delta_t)):
         r[t,i] = ogive(0,0.5,t*delta_t - 1.5)
+
+#define subset S for experiment 1
+#either 0,3,6,9,12
+S = np.array([0,1,2])
+
+#make the perturbed set speed up after total of 5 seconds
+perturb(S, 5, Heading = True, Speed = True)
+
+#get position of virtual neighbours from the perturbed speed and heading
+for t in range(1, len(t_set)):
+    for p in range(n):
+            distance        = r * delta_t
+            position[t,p,0] = position[t-1,p,0] + distance[t-1,p]*np.cos(phi[t-1,p])
+            position[t,p,1] = position[t-1,p,1] + distance[t-1,p]*np.sin(phi[t-1,p])
+
+
+#set all initial positions randomly
 '''
 for i in range(n):
     position[0,i,0] = 0.5*random.random()
@@ -70,16 +91,18 @@ for i in range(n):
     r[0,i]   = 0.3* random.random()
 '''
 
+#move according to model
+'''
 for t in range(1, len(t_set)):
     for p in range(n):
         r[t,p]   = r[t-1,p]
         phi[t,p] = phi[t-1,p]
-        localN = 0
-        phi_sum = 0
-        r_sum   = 0
+        localN   = 0
+        phi_sum  = 0
+        r_sum    = 0
         for i in range(n):
-            dis     = d(i,p,t-1)
-            ang     = angle(i,p,t-1)
+            dis = d(i,p,t-1)
+            ang = angle(i,p,t-1)
             if (dis< d_max) and (np.absolute(ang)<angle_max) and (i != p):#need to check but I think the angle is right now
                 localN  += 1
                 w_i      = w(i,p,t-1)
@@ -88,22 +111,22 @@ for t in range(1, len(t_set)):
         if localN > 0:
             r[t,p]   = r[t-1,p]   + delta_t * (c/localN) * r_sum
             phi[t,p] = phi[t-1,p] - delta_t * (k/localN) * phi_sum
-        distance = r * delta_t
+        distance        = r * delta_t
         position[t,p,0] = position[t-1,p,0] + distance[t-1,p]*np.cos(phi[t-1,p])
         position[t,p,1] = position[t-1,p,1] + distance[t-1,p]*np.sin(phi[t-1,p])
-
+'''
 for i in range(n):
     plt.scatter(position[:,i,0],position[:,i,1])
-my_quiver = np.zeros((len(t_set), n, 2))
-for t in range(len(t_set)-1):
-    for p in range(n):
-        my_quiver[t,p,0] = -position[t,p,0] + position[t+1,p,0]
-        my_quiver[t,p,1] = -position[t,p,1] + position[t+1,p,1]
-my_quiver = my_quiver/200
-for i in range(n):
-    plt.quiver(position[::4,i,0], position[::4,i,1],my_quiver[::4,i,0], my_quiver[::4,i,1], units = 'width')
+#my_quiver = np.zeros((len(t_set), n, 2))
+#for t in range(len(t_set)-1):
+#    for p in range(n):
+#        my_quiver[t,p,0] = -position[t,p,0] + position[t+1,p,0]
+#        my_quiver[t,p,1] = -position[t,p,1] + position[t+1,p,1]
+#my_quiver = my_quiver/200
+#for i in range(n):
+#    plt.quiver(position[::4,i,0], position[::4,i,1],my_quiver[::4,i,0], my_quiver[::4,i,1], units = 'width')
 
-#plt.show()
+plt.show()
 
 #read in data from the paper
 exp1_LateralDeviation_strip = pd.read_csv('/home/ellie/Documents/M4R/exp1/Exp1_LateralDeviation_strip.txt', sep="\s+", skip_blank_lines=True, 
