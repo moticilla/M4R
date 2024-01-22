2a5917725cee5a2c208edcbe98437a6a667086ea
moticilla <ellie.pizey@gmail.com>
2024-01-22 21:45:19 +0000

    Created plots

--- RioCode.py
+++ RioCode.py
@@ -5,7 +5,7 @@ import pandas as pd
 from scipy import special
 #Variables:
 delta_t = 0.05
-t_max = 20
+t_max = 15
 n = 31
 d_max = 5
 pi = 3.14
@@ -79,7 +79,7 @@ def move_person():
                 r_sum   += w_i *(r[t-1,i]-r[t-1,p])
         if localN > 0:
             r[t,p]   = r[t-1,p]   + delta_t * (c/localN) * r_sum
-            phi[t,p] = phi[t-1,p] - delta_t * (k/localN) * phi_sum  #should this be a plus?? It behaves a lot more nicely when it is.
+            phi[t,p] = phi[t-1,p] + delta_t * (k/localN) * phi_sum
         distance        = r * delta_t
         total_distance= np.sum(distance[:,30])
         position[t,p,0] = position[t-1,p,0] + distance[t-1,p]*np.cos(phi[t-1,p])
@@ -93,13 +93,13 @@ for i in range(30):
     for t in range(int(3//delta_t)):
         r[t,i] = 1.3*ogive(0,0.5,t*delta_t - 1.5)
 
-my_exp1_final_heading_data = [[1,2,3,0,0,0,0,0,0,0]]
-def all_together(i,S):
+my_exp1_final_heading_data = [[1,2,3,0,0,0,0,0,0,0],[1,2,3,0,0,0,0,0,0,0]]
+def all_together(i,S, h=True, speed =False):
     global position
     global phi
     global r
     #make the perturbed set change heading up after total of 5 seconds
-    perturb(S, 5, sign=-1,Heading = True, Speed = False)
+    perturb(S, 5,Heading = h, Speed = speed)
     #get position of virtual neighbours from the perturbed speed and heading
     for t in range(1, len(t_set)):
         for p in range(n):
@@ -113,51 +113,86 @@ def all_together(i,S):
     while r[final_t,30] >0 and final_t<1000:
         final_t+=1
     #update dataframe
-    my_exp1_final_heading_data[0][i]=phi[final_t-1,30]
+    if h == True:
+        my_exp1_final_heading_data[0][i]=phi[final_t-1,30]*360/(2*pi)
+    if speed ==True:
+        my_exp1_final_heading_data[1][i]=r[final_t-1,30]
     #un perturb virtual people by speed
     for i in range(30):
         for t in range(int(3//delta_t),len(t_set)):
             r[t,i] = 1.3
         for t in range(int(3//delta_t)):
             r[t,i] = 1.3*ogive(0,0.5,t*delta_t - 1.5)
+    for i in range(len(t_set)):
+        r[i,30]=0
     #un perturb by heading
-    phi      = np.zeros((len(t_set),n))
-#define subset S for experiment 1 Near 0,Far 0
+    for i in range(30):
+        phi[:,i] = 0
+#define subset S for experiment 1 Near 0,Far 0, heading
 S = np.array([])
 all_together(0,S)
 all_together(1,S)
-#define subset S for experiment 1 Near 3
+#define subset S for experiment 1 Near 3, heading
 S = np.array([0,1,2])
 all_together(2,S)
-#define subset S for experiment 1 Near 6
+#define subset S for experiment 1 Near 6, heading
 S = np.array([0,1,2,3,4,5])
 all_together(3,S)
-#define subset S for experiment 1 Near 9
+#define subset S for experiment 1 Near 9, heading
 S = np.array([0,1,2,3,4,5,6,7,8])
 all_together(4,S)
-#define subset S for experiment 1 Near 12
+#define subset S for experiment 1 Near 12, heading
 S = np.array([0,1,2,3,4,5,6,7,8,9,10,11])
 all_together(5,S)
-#define subset S for experiment 1 Far 3
-S = np.array([12,13,14])
+#define subset S for experiment 1 Far 3, heading
+S = np.array([14,15,16])
 all_together(6,S)
-#define subset S for experiment 1 Far 6
-S = np.array([12,13,14,15,16,17])
+#define subset S for experiment 1 Far 6, heading
+S = np.array([14,15,16,17,18,19])
 all_together(7,S)
-#define subset S for experiment 1 Far 9
-S = np.array([12,13,14,15,16,17,18,19,10])
+#define subset S for experiment 1 Far 9, heading
+S = np.array([14,15,16,17,18,19,20,21,22])
 all_together(8,S)
-#define subset S for experiment 1 Far 12
-S = np.array([12,13,14,15,16,17,18,19,20,21,22,23])
+#define subset S for experiment 1 Far 12, heading
+S = np.array([14,15,16,17,18,19,20,21,22,23,24,25,26])
 all_together(9,S)
+
+#define subset S for experiment 1 Near 0,Far 0, speed
+S = np.array([])
+all_together(0,S,h=False, speed =True)
+all_together(1,S,h=False, speed =True)
+#define subset S for experiment 1 Near 3, speed
+S = np.array([0,1,2])
+all_together(2,S,h=False, speed =True)
+#define subset S for experiment 1 Near 6, speed
+S = np.array([0,1,2,3,4,5])
+all_together(3,S,h=False, speed =True)
+#define subset S for experiment 1 Near 9, speed
+S = np.array([0,1,2,3,4,5,6,7,8])
+all_together(4,S,h=False, speed =True)
+#define subset S for experiment 1 Near 12, speed
+S = np.array([0,1,2,3,4,5,6,7,8,9,10,11])
+all_together(5,S,h=False, speed =True)
+#define subset S for experiment 1 Far 3, speed
+S = np.array([14,15,16])
+all_together(6,S,h=False, speed =True)
+#define subset S for experiment 1 Far 6, speed
+S = np.array([14,15,16,17,18,19])
+all_together(7,S,h=False, speed =True)
+#define subset S for experiment 1 Far 9, speed
+S = np.array([14,15,16,17,18,19,20,21,22])
+all_together(8,S,h=False, speed =True)
+#define subset S for experiment 1 Far 12, speed
+S = np.array([14,15,16,17,18,19,20,21,22,23,24,25,26])
+all_together(9,S,h=False, speed =True)
+
 
 #make dataFrame
 my_exp1_final_heading = pd.DataFrame(my_exp1_final_heading_data, columns=['Near0', 'Far0','Near3','Near6','Near9',
                                                                           'Near12','Far3','Far6','Far9','Far12'])
-print(my_exp1_final_heading)
-print(my_exp1_final_heading_data[0][4])
-for i in range(n):
-    plt.scatter(position[:,i,0],position[:,i,1], s=5)
+#for i in range(n):
+    #plt.scatter(position[:,i,0],position[:,i,1], s=5)
+
 #my_quiver = np.zeros((len(t_set), n, 2))
 #for t in range(len(t_set)-1):
 #    for p in range(n):
@@ -167,7 +202,7 @@ for i in range(n):
 #for i in range(n):
 #    plt.quiver(position[::4,i,0], position[::4,i,1],my_quiver[::4,i,0], my_quiver[::4,i,1], units = 'width')
 
-plt.show()
+#plt.show()
 
 #read in data from the paper
 exp1_LateralDeviation_strip = pd.read_csv('/home/ellie/Documents/M4R/exp1/Exp1_LateralDeviation_strip.txt', 
@@ -176,12 +211,55 @@ exp1_LateralDeviation_strip = pd.read_csv('/home/ellie/Documents/M4R/exp1/Exp1_L
 exp1_LateralDeviation_strip.columns = ["Subject", "Near 0", "Far 0", "Near 3", "Near 6", "Near 9", "Near 12", 
                                        "Far 3", "Far 6", "Far 9", "Far 12"]
 exp1_LateralDeviation_strip.drop([10,13,14,25,28,29,40], axis = 0)
-#print(exp1_LateralDeviation_strip)
-t = 5
-x = [t_step*delta_t for t_step in range(int(0.5//delta_t))]
-y = [(10/360)*2*pi*ogive(0,0.083,t_step*delta_t-0.25) for t_step in range(int(0.5//delta_t))]
 
-x1 = np.arange(-5,5,0.01)
-y1 = ogive(3,0.083,x1)
-#plt.plot(x1,y1)
-#plt.show()
+
+exp1_final_heading = pd.read_csv('/home/ellie/Documents/M4R/exp1/Simulation_Exp1_FinalHeading.txt', 
+                                          sep="\s+", skip_blank_lines=True, 
+                                          header=None, skiprows = [0,1,12,15,17])
+exp1_final_heading.columns = ["Subject", "Near0", "Far0", "Near3", "Near6", "Near9", "Near12", 
+                                       "Far3", "Far6", "Far9", "Far12"]
+
+exp1_final_speed = pd.read_csv('/home/ellie/Documents/M4R/exp1/Simulation_Exp1_FinalSpeed.txt', 
+                                          sep="\s+", skip_blank_lines=True, 
+                                          header=None, skiprows = [0,1,12,15,17])
+exp1_final_speed.columns = ["Subject", "Near0", "Far0", "Near3", "Near6", "Near9", "Near12", 
+                                       "Far3", "Far6", "Far9", "Far12"]
+
+x = [0,3,6,9,12]
+y_near_speed = [exp1_final_speed.Near0[11],exp1_final_speed.Near3[11],exp1_final_speed.Near6[11],exp1_final_speed.Near9[11],exp1_final_speed.Near12[11]]
+y_far_speed = [exp1_final_speed.Far0[11],exp1_final_speed.Far3[11],exp1_final_speed.Far6[11],exp1_final_speed.Far9[11],exp1_final_speed.Far12[11]]
+y_near_heading = [exp1_final_heading.Near0[11],exp1_final_heading.Near3[11],exp1_final_heading.Near6[11],exp1_final_heading.Near9[11],exp1_final_heading.Near12[11]]
+y_far_heading = [exp1_final_heading.Far0[11],exp1_final_heading.Far3[11],exp1_final_heading.Far6[11],exp1_final_heading.Far9[11],exp1_final_heading.Far12[11]]
+
+plt.subplot(2, 1, 1)
+plt.plot(x,y_near_heading,color = 'b')
+plt.plot(x,y_far_heading,color = 'r')
+plt.title('Final headings and speeds from paper')
+plt.ylabel('Final lateral deviation')
+
+plt.subplot(2, 1, 2)
+plt.plot(x,y_near_speed,color = 'b')
+plt.plot(x,y_far_speed,color = 'r')
+plt.ylabel('Final change in speed')
+plt.xlabel('Number of perturbed neighours')
+
+plt.show()
+
+y_near_heading = [my_exp1_final_heading_data[0][i] for i in [0,2,3,4,5]]
+y_far_heading = [my_exp1_final_heading_data[0][i] for i in [1,6,7,8,9]]
+y_near_speed = [my_exp1_final_heading_data[0][i]-1.3 for i in [0,2,3,4,5]]
+y_far_speed = [my_exp1_final_heading_data[0][i]-1.3 for i in [1,6,7,8,9]]
+
+plt.subplot(2, 1, 1)
+plt.plot(x,y_near_heading,color = 'b')
+plt.plot(x,y_far_heading,color = 'r')
+plt.title('My Final headings and speeds')
+plt.ylabel('Final lateral deviation')
+
+plt.subplot(2, 1, 2)
+plt.plot(x,y_near_speed,color = 'b')
+plt.plot(x,y_far_speed,color = 'r')
+plt.ylabel('Final change in speed')
+plt.xlabel('Number of perturbed neighours')
+
+plt.show()
